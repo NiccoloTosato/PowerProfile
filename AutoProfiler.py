10f24d3122e0f2e4eed5f2e5d9d8ec1083809b6e
Niccolo Tosato <niccolo.tosato@yahoo.it>
2023-11-10 15:08:05 +0100

    Update file name

--- AutoProfiler.py
+++ AutoProfiler.py
@@ -7,7 +7,6 @@ from subprocess import Popen, PIPE
 import matplotlib.pyplot as plt
 
 class AutoPowerZone:
-
     def __init__(self,path,interface):
         self.path=path
         self.interface=interface
@@ -52,7 +51,7 @@ class AutoPowerZone:
         return value
     
     def __repr__(self):
-        s=f"{self.name} {self.read_energy()} uj"
+        s=f"{self.name}\t{self.read_energy()} uj"
         return s
     
     def allocate(self):
@@ -62,6 +61,8 @@ class AutoPowerZone:
     def store_read(self):
         self.data.append(self.read_energy())
         return
+    def reset(self):
+        self.allocate()
             
     @staticmethod
     def autodetect(interface='intel-rapl'):
@@ -78,24 +79,33 @@ class AutoPowerZone:
         return powerzones
 
 class AutoProfiler:
-    def __init__(self,powerzones=None, command=None,args=None, dt=0.1):
+    def __init__(self,powerzones=None, command=None,args=None, dt=0.1,filename="default.hdf5"):
         self.dt=dt
         #Timestep in nS
         self.dtns=dt*1e9
-        if (powerzones is not None) and (path is not None): 
+        self.filename=filename
+        if (command is not None): 
             # I can start profiling ! 
             self.powerzones=powerzones
-            self.path=path
-
+            if len(powerzones)>0:
+                print("Detected powerzones:\n[name]\t[current read]")
+                for zone in self.powerzones:
+                    print(zone)
+            else:
+                print("No powerzones detected")
+            self.command=command
+            self.args=args
             #How many time the process sleep between each cicle, higher the value, more precise is the measure
             self.sleeptime=dt/10
             self.time_dict=None
-            for zone in self.powerzones:
-                print(zone)
+            print(f"Profiler command: {self.command}, args {self.args}, dt {self.dt}")
+
+
                 
     def start(self):
         #lanciamo un altro programma ! poi profiliamo fintanto che sto coso gira, poi ci fermiamo.
-        process=Popen(['/usr/bin/sleep', '4'], stdout=PIPE, stderr=PIPE)
+        self.reset()
+        process=Popen([self.command, self.args], stdout=PIPE, stderr=PIPE)
         init_time=last_time=time.time_ns()
         while process.poll() is None:
             while (time.time_ns()-last_time < self.dtns):
@@ -104,9 +114,12 @@ class AutoProfiler:
             last_time=time.time_ns()
         #qui ho finito di profilare
         #self.time_dict=self.to_timedict()
-        self.save()
+        print("Command output:")
+        print(process.stdout.read())
+        self.save(filename=self.filename)
+        print(f"Profiling time: {round((last_time-init_time)/(10**9),2)} [s]")
         print("End profiling")
-        
+
     def cycle(self):
         for zone in self.powerzones:
             zone.store_read()
@@ -116,7 +129,7 @@ class AutoProfiler:
             zone.reset()
             
     def __repr__(self):
-        s=f"Profile time: {99999999999} s, N* samples {9999999}, dt {self.dt} s\nZones:\n"
+        s=f"Profiler command: {self.command}, args {self.args}, dt {self.dt} [s]\nZones:\n"
         for zone in self.powerzones:
             s+=f"\t{zone.name}\n"
         return s   
@@ -136,15 +149,20 @@ class AutoProfiler:
         #        name+=f"_{i}"
         #        i+=1
         #    timedict[name]=TimeSeries(power,dx=self.dt,unit="Watts",name=zone.name)
-    def plot_profile(self,filename="default.hdf5"):
-        self.profiles=dict()
-        plt.figure()
-        with h5py.File(filename, 'r') as f:
-            for zone_group in f.keys():
-                for zone_dataset in f[zone_group].keys():
-                    print(f[zone_group][zone_dataset])         
-                    plt.plot(f[zone_group][zone_dataset][:],label=f"{zone_group}/{zone_dataset}")
-            plt.legend()
+    def plot_profile(self,filename=None):
+        if self.filename is not None:
+            self.profiles=dict()
+            plt.figure(figsize=(16,7),dpi=300)
+            with h5py.File(self.filename, 'r') as f:
+                for zone_group in f.keys():
+                    for zone_dataset in f[zone_group].keys():
+                        dataset=f[zone_group][zone_dataset]
+                        x_axis=dataset.attrs['dt']*np.arange(0,len(dataset))
+                        plt.plot(x_axis,dataset[:],label=f"{zone_group}/{zone_dataset}")
+                plt.legend()
+                plt.ylabel ("Power [W]")
+                plt.xlabel("Time [s]")
+                plt.savefig("results.png")
     '''
     def plot(self):
         plot=self.time_dict.plot()
